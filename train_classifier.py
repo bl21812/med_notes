d3378e54fa44049006636a97c64f319c6a3e1a88
brian <b384li@uwaterloo.ca>
2023-06-08 12:00:30 -0400

    some testing

--- train_classifier.py
+++ train_classifier.py
@@ -4,6 +4,12 @@ from accelerate import infer_auto_device_map, init_empty_weights
 tokenizer = AutoTokenizer.from_pretrained("medalpaca/medalpaca-13b", device_map="auto")
 model = AutoModelForCausalLM.from_pretrained("medalpaca/medalpaca-13b", device_map="auto")
 
+print(tokenizer)
 print(model)
 
-# dummy utterance?
+test_sent = 'This is a sentence with cardiovascular issues'
+
+embed = model.model.embed_tokens
+
+print(embed)
+
