82d6e607d4ec94a7d84d8debeec0445c174a8cc2
brian <b384li@uwaterloo.ca>
2023-06-08 00:29:59 -0400

    remove import

--- train_classifier.py
+++ train_classifier.py
@@ -1,6 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from accelerate import infer_auto_device_map, init_empty_weights
-from torchsummary import summary
 
 tokenizer = AutoTokenizer.from_pretrained("medalpaca/medalpaca-13b", device_map="auto")
 model = AutoModelForCausalLM.from_pretrained("medalpaca/medalpaca-13b", device_map="auto")
