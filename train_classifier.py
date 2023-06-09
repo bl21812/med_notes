1a6885debc5eb75671e83199b7ccbe827cc12d55
brian <b384li@uwaterloo.ca>
2023-06-08 21:12:19 -0400

    oops

--- train_classifier.py
+++ train_classifier.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
-from .classifier import Classifier
+from classifier import Classifier
 
 tokenizer = AutoTokenizer.from_pretrained("medalpaca/medalpaca-13b", device_map="auto")
 alpaca = AutoModelForCausalLM.from_pretrained("medalpaca/medalpaca-13b", device_map="auto")
