6962c460fa0d3b08766d3e2277481188780034ad
brian <b384li@uwaterloo.ca>
2023-06-24 20:14:38 -0400

    shape

--- data_utils.py
+++ data_utils.py
@@ -33,6 +33,7 @@ def tokenize_qa(tokenizer, x1, x2=None, max_seq_length=2048, doc_stride=128, pad
 
     # extract token ids
     # TODO: is this how it works for batches ?
-    ids = [[[token_seq for token_seq in token_seq_list] for token_seq_list in tokenized['input_ids']]]
+    # ids = [[[token_seq for token_seq in token_seq_list] for token_seq_list in tokenized['input_ids']]]
+    ids = [[token_seq for token_seq in tokenized['input_ids']]]
 
     return ids
