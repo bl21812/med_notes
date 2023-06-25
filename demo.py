65d7d624c65cb7b5de9f0c81d3cc8b2d1ec5f1dd
brian <b384li@uwaterloo.ca>
2023-06-25 16:29:11 -0400

    device

--- demo.py
+++ demo.py
@@ -79,7 +79,7 @@ else:
     with init_empty_weights():
         model = AutoModelForCausalLM.from_config(config)
     model.tie_weights()
-    max_memory = {0: "0GIB", 1: "8GIB", 2: "8GIB", 3: "8GIB"}  # avoid GPU 0
+    max_memory = {0: "0GIB", 1: "10GIB", 2: "10GIB", 3: "10GIB"}  # avoid GPU 0
     device_map = infer_auto_device_map(model, max_memory=max_memory)
     model = AutoModelForCausalLM.from_pretrained(model_source, device_map=device_map, offload_folder='offload')
     '''model = load_checkpoint_and_dispatch(
