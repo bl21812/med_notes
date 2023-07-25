a64c456d0188348d3ca4ff721a390546254d7a69
brian <b384li@uwaterloo.ca>
2023-07-25 17:36:39 -0400

    testing

--- finetune_adapter.py
+++ finetune_adapter.py
@@ -8,15 +8,18 @@ model = AutoModelForSeq2SeqLM.from_pretrained(
     base_model_source, 
     device_map='auto'
 )
-# print(model)
 
 config = ParallelConfig(
     mh_adapter=True,
-    output_adapter=True,
+    output_adapter=True,  # can keep both of these in for now (unsure if needed)
     reduction_factor=16,  # important param !! (not sure what val)
     non_linearity="relu"
 )
 model.add_adapter("bottleneck_adapter", config=config)
-print(model)
+
+print(model.config.adapters)
+
+# model.train_adapter()
+# print trainable params
 
 input()
