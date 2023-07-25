a56b13206309048c74bdc0cde0d6d0bd82020ac3
brian <b384li@uwaterloo.ca>
2023-07-25 17:31:03 -0400

    adapter

--- finetune_adapter.py
+++ finetune_adapter.py
@@ -1,4 +1,4 @@
-from transformers.adapters import AdapterConfig
+from transformers.adapters import ParallelConfig
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 tokenizer_source = "knkarthick/meeting-summary-samsum"
@@ -8,7 +8,15 @@ model = AutoModelForSeq2SeqLM.from_pretrained(
     base_model_source, 
     device_map='auto'
 )
+# print(model)
 
+config = ParallelConfig(
+    mh_adapter=True,
+    output_adapter=True,
+    reduction_factor=16,  # important param !! (not sure what val)
+    non_linearity="relu"
+)
+model.add_adapter("bottleneck_adapter", config=config)
 print(model)
 
 input()
