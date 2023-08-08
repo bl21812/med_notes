3a51a659926fca638ea3eef115b3e889bb33f432
brian <b384li@uwaterloo.ca>
2023-08-08 17:13:32 -0400

    verbosity

--- finetune_adapter.py
+++ finetune_adapter.py
@@ -137,7 +137,7 @@ training_args =  Seq2SeqTrainingArguments(
     load_best_model_at_end=True,
     metric_for_best_model='rougeLsum',
     output_dir=save_path,
-    logging_strategy='no',
+    log_level='critical',
 )
 
 trainer = Seq2SeqTrainer(
