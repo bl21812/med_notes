5db29e1a9903b107da46bdf6783140928965f4b4
brian <b384li@uwaterloo.ca>
2023-09-27 16:02:42 -0400

    testing

--- finetune_bert_classifier.py
+++ finetune_bert_classifier.py
@@ -68,6 +68,7 @@ ds_val = ds['test']
 # convert back to string labels
 def label_to_str(row):
     row[label_key] = ds_train.features[label_key].int2str(row[label_key])
+    print(row)
     return row
 
 ds_train = ds_train.map(label_to_str)
